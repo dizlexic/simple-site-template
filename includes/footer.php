9f38d4158fe767a1217731cd8373827297b20b35
dizlexic <dmoore1987@gmail.com>
2016-01-14 08:14:03 -0600

    updated jquery include

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
   <footer>
 
   </footer>
-  <script type="text/javascript" src="/js/vendor/jquery-1.11.3.min.js"></script>
+  <script type="text/javascript" src="/js/vendor/jquery-1.12.0.min.js"></script>
   <script type="text/javascript" src="/js/main.js"></script>
 </body>
 </html>
